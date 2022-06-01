90615e87816f0c402c37c1d38963f2d8ec113112
Sylvia-Chepkoech <slycheche@gmail.com>
2022-06-01 10:59:02 +0300

    initialize view binding

--- app/src/main/java/com/example/spiceit_app/LogInActivity.java
+++ app/src/main/java/com/example/spiceit_app/LogInActivity.java
@@ -3,8 +3,14 @@ package com.example.spiceit_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LogInActivity extends AppCompatActivity {
+    @BindView(R.id.loginbutton) Button mLogInButton;
+    @BindView(R.id.PersonName) EditText mPersonName;
+    @BindView(R.id.password) EditText mPassword;
+    @BindView(R.id.confirmpass) EditText mConfirmPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
