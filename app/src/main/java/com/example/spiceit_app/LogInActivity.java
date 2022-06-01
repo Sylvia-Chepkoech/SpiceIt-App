1b6c75c9e8095000897a495cb1a6a8f4eb2b32d0
Sylvia-Chepkoech <slycheche@gmail.com>
2022-06-01 11:17:25 +0300

    implement View.OnClickListener interface

--- app/src/main/java/com/example/spiceit_app/LogInActivity.java
+++ app/src/main/java/com/example/spiceit_app/LogInActivity.java
@@ -3,13 +3,14 @@ package com.example.spiceit_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class LogInActivity extends AppCompatActivity {
+public class LogInActivity extends AppCompatActivity implements View.OnClickListener {
     @BindView(R.id.loginbutton) Button mLogInButton;
     @BindView(R.id.PersonName) EditText mPersonName;
     @BindView(R.id.password) EditText mPassword;
@@ -20,5 +21,13 @@ public class LogInActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
         ButterKnife.bind(this);
+
+        mLogInButton.setOnClickListener(this);
+    }
+            @Override
+            public void onClick(View v) {
+
+
+        };
     }
 }
