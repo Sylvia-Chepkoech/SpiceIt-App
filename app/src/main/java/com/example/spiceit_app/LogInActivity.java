86562319a6bb8cc8160ec4f60c06bf8f2efef944
Sylvia-Chepkoech <slycheche@gmail.com>
2022-06-01 11:06:04 +0300

    integrate butterknife'

--- app/src/main/java/com/example/spiceit_app/LogInActivity.java
+++ app/src/main/java/com/example/spiceit_app/LogInActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class LogInActivity extends AppCompatActivity {
     @BindView(R.id.loginbutton) Button mLogInButton;
     @BindView(R.id.PersonName) EditText mPersonName;
@@ -16,5 +19,6 @@ public class LogInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        ButterKnife.bind(this);
     }
 }
