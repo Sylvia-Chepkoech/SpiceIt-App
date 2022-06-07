f288a3f74684928d90d25af9be42cc625d39a341
Sylvia-Chepkoech <slycheche@gmail.com>
2022-06-07 14:00:09 +0300

    create java class that rep response

---
+++ app/src/main/java/com/example/spiceit_app/models/RandomCocktails.java
@@ -0,0 +1,40 @@
+
+package com.example.spiceit_app.models;
+
+import java.util.List;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+
+public class RandomCocktails {
+
+    @SerializedName("drinks")
+    @Expose
+    private List<Drink> drinks = null;
+
+    /**
+     * No args constructor for use in serialization
+     * 
+     */
+    public RandomCocktails() {
+    }
+
+    /**
+     * 
+     * @param drinks
+     */
+    public RandomCocktails(List<Drink> drinks) {
+        super();
+        this.drinks = drinks;
+    }
+
+    public List<Drink> getDrinks() {
+        return drinks;
+    }
+
+    public void setDrinks(List<Drink> drinks) {
+        this.drinks = drinks;
+    }
+
+}
