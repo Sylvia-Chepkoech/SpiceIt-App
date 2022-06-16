c6a93a70136eef090430aceb92c3711d844ace54
Sylvia-Chepkoech <slycheche@gmail.com>
2022-06-16 14:52:05 +0300

    bind views

--- app/src/main/java/com/example/spiceit_app/ui/FavoriteDrinkActivity.java
+++ app/src/main/java/com/example/spiceit_app/ui/FavoriteDrinkActivity.java
@@ -1,13 +1,34 @@
 package com.example.spiceit_app.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import com.example.spiceit_app.R;
+import com.example.spiceit_app.adapters.MealRecyclerAdapter;
+import com.example.spiceit_app.models.Constants;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import butterknife.BindView;
 
 public class FavoriteDrinkActivity extends AppCompatActivity {
+    @BindView(R.id.recyclerView)
+    RecyclerView myRecycler;
+    @BindView(R.id.progressBar)
+    ProgressBar progress;
+    MealRecyclerAdapter adapt;
+    RecyclerView.LayoutManager layoutManager;
+    DatabaseReference reference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        reference = FirebaseDatabase.getInstance().getReference().child(Constants.DRINKS);
+
     }
 }
