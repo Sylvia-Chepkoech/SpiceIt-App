60eaaf68817e2d4eff53680751ac5de438fcdd07
Sylvia-Chepkoech <slycheche@gmail.com>
2022-06-16 14:39:36 +0300

    create activity

---
+++ app/src/main/java/com/example/spiceit_app/ui/FavoriteDrinkActivity.java
@@ -0,0 +1,13 @@
+package com.example.spiceit_app.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FavoriteDrinkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
